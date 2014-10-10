0439688d4018e78a6e1d6248f71fd35d4f429637
Ryan Prichard <ryan.prichard@gmail.com>
2014-10-09 22:05:59 -0700

    Style change: Remove trailing whitespace and put func brackets on own line.

--- ObjectiveCBridge.m
+++ ObjectiveCBridge.m
@@ -2,7 +2,7 @@
 // critical failing, because the C++ flags can alter the library ABI.  In
 // particular, -stdlib=libc++ enables an incompatible standard library.
 //
-// I do not see an obvious non-hacky fix for this issue, and without one, 
+// I do not see an obvious non-hacky fix for this issue, and without one,
 // Objective-C++ is unusable with qmake, at least on OS X with the version of
 // Qt I'm using (Qt 4.8.6 installed via Homebrew).
 //
@@ -13,46 +13,54 @@
 #import <AppKit/AppKit.h>
 #import <Foundation/Foundation.h>
 
-id cursor_currentSystemCursor() {
+id cursor_currentSystemCursor()
+{
     return [NSCursor currentSystemCursor];
 }
 
-BridgePoint cursor_hotSpot(id cursor) {
+BridgePoint cursor_hotSpot(id cursor)
+{
     NSPoint ret = [(NSCursor*)cursor hotSpot];
     BridgePoint ret2 = { ret.x, ret.y };
     return ret2;
 }
 
-BridgePoint event_mouseLocation() {
+BridgePoint event_mouseLocation()
+{
     NSPoint ret = [NSEvent mouseLocation];
     BridgePoint ret2 = { ret.x, ret.y };
     return ret2;
 }
 
-id cursor_image(id cursor) {
+id cursor_image(id cursor)
+{
     return [(NSCursor*)cursor image];
 }
 
-id image_TIFFRepresentation(id image) {
+id image_TIFFRepresentation(id image)
+{
     return [(NSImage*)image TIFFRepresentation];
 }
 
-const void *data_bytes(id data) {
+const void *data_bytes(id data)
+{
     return [(NSData*)data bytes];
 }
 
-size_t data_length(id data) {
+size_t data_length(id data)
+{
     return [(NSData*)data length];
 }
 
-void bridgeWriteCursorFile(id tiffData, uint32_t imageID) {
-    NSBitmapImageRep *imageRep = 
-        [[[NSBitmapImageRep alloc] 
+void bridgeWriteCursorFile(id tiffData, uint32_t imageID)
+{
+    NSBitmapImageRep *imageRep =
+        [[[NSBitmapImageRep alloc]
             initWithData:(NSData*)tiffData] autorelease];
     assert(imageRep != nil);
-    NSData *pngData = 
+    NSData *pngData =
         [imageRep representationUsingType:NSPNGFileType properties:nil];
-    NSString *path = 
+    NSString *path =
         [NSString stringWithFormat:@"cursor_%u.png", imageID];
     [pngData writeToFile:path atomically:NO];
 }
